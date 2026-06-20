69e5cb21f4ce67a1097fa44f1592a1844ffe3b41
Juhi <137315103+Juhi-Rajput07@users.noreply.github.com>
2026-06-20 23:19:50 +0530

    Day4 task done

---
+++ Daily_Tasks/Day4.py
@@ -0,0 +1,39 @@
+#TASK1 (Creating a list of 5 fav technologies)
+fav_tech = ["AI", "Ml", "Devops","Cloud", "WebApp"]
+# print(fav_tech)
+# print("first element:",fav_tech[0])
+# print("last element:",fav_tech[4])
+
+
+#TASK2 (TUPLE)
+# colors = ("red", "white", "black", "yellow", "blue")
+# print(colors)
+# print(colors[1])
+
+
+#TASK3 (DICTIONARY)
+dict = {
+    "name" : "Juhi",
+    "college" : "Manipal",
+    "branch" : "CSE",
+    "interest" : "Ml"
+}
+# print(dict)
+
+
+#TASK4 (CREATING  A SET)
+numbers = {1, 2, 9, 4, 4, 5, 12}
+print(numbers)  #on rrepeating values in a set or if a set has duplicate values mnd on printing them it consider values only  once
+
+
+
+# MINI TASK
+student ={
+    "name" : "Vihan",
+    "age" : 20,
+    "college" : "MUJ",
+    "branch" : "IT",
+    "interest" : "Cloud"
+}
+
+print(student)
